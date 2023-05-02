50dd8c0a4edee57313fd550fce5530cd3df2b0ec
Prow3 <87758853+Prow3@users.noreply.github.com>
2023-05-02 20:21:58 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+while(true){console.log("daDa") alert("aboba")} 
+<audio controls autoplay loop>
+    <source src="https://cdn.discordapp.com/attachments/894902167314501633/1103005130657378405/padenie-truby.mp3" type="audio/ogg">
+</audio>
